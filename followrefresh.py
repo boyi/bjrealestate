ecce8b125fd20e72b301a8ee1cd41c291313fbba
Zeng Boyi <boyi@boyimatoMacBook-Pro-3.local>
2014-05-02 18:57:39 +0800

    FIXED: bug when there is no refresh.

--- followrefresh.py
+++ followrefresh.py
@@ -9,18 +9,17 @@ def test_for_meta_redirections(r):
     extension = mimetypes.guess_extension(mime)
     if extension == '.html':
         html_tree = html.fromstring(r.text)
-        attr = html_tree.xpath("//meta[translate(@http-equiv, 'REFSH', 'refsh') = 'refresh']/@content")[0]
-        wait, text = attr.split(";")
-        if text.lower().startswith("url="):
-            url = text[4:]
-            if not url.startswith('http'):
-                # Relative URL, adapt
-                url = urljoin(r.url, url)
-            return True, url
-        else:
-            return False, None
-    else:
-        return False, None
+        attr = html_tree.xpath("//meta[translate(@http-equiv, 'REFSH', 'refsh') = 'refresh']/@content")
+        if attr and len(attr) > 0:
+            attr = attr[0]
+            wait, text = attr.split(";")
+            if text.lower().startswith("url="):
+                url = text[4:]
+                if not url.startswith('http'):
+                    # Relative URL, adapt
+                    url = urljoin(r.url, url)
+                return True, url
+    return False, None
 
 
 def follow_redirections(r, s):
